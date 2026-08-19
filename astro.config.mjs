b31adc877ed61c4e8fad944ad3122d288b1cd50a
alealessandrini1995 <ale.alessandrini1995@gmail.com>
2026-08-19 17:42:26 +0200

    Correzione astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // Fase dev: pubblicato su GitHub Pages come project site, non ancora sul dominio definitivo.
-  site: 'https://alessandrini1995.github.io',
+  site: 'https://alealessandrini1995.github.io',
   base: '/egslab-web-site',
   output: 'static',
   integrations: [sitemap()],
